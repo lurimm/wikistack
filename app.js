df8d3918b62ef50dc02a2156e7b734945f6cdcf6
Luis Rincon <rincon_92@hotmail.com>
2018-01-19 17:07:28 -0500

    started server

---
+++ app.js
@@ -0,0 +1,18 @@
+'use strict';
+const express = require('express');
+const nunjucks = require('nunjucks');
+const bodyParser = ('body-parser');
+const morgan = ('morgan');
+
+const app = express();
+
+app.set('view engine', 'html'); // have res.render work with html files
+app.engine('html', nunjucks.render); // when giving html files to res.render, tell it to use nunjucks
+nunjucks.configure('views', { noCache: true }) // point nunjucks to the proper directory for templates
+
+app.listen(3000, () => console.log('im listening'));
+app.use(morgan());
+
+app.get('/', (req, res, next) => {
+    res.render('index');
+})
