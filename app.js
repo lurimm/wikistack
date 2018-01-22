e9a3d0ca50f4b0869d2e6170aa681778dc0cc3b6
Luis Rincon <rincon_92@hotmail.com>
2018-01-22 10:46:04 -0500

    database synced

--- app.js
+++ app.js
@@ -3,18 +3,22 @@ const express = require('express');
 const nunjucks = require('nunjucks');
 const bodyParser = ('body-parser');
 const morgan = ('morgan');
+const models = require('./models');
 
 const app = express();
 
 app.set('view engine', 'html'); // have res.render work with html files
 app.engine('html', nunjucks.render); // when giving html files to res.render, tell it to use nunjucks
-
 nunjucks.configure('views', { noCache: true }) // point nunjucks to the proper directory for templates
-
 app.use(express.static('public')) //serve up static files
 
-
-app.listen(3000, () => console.log('im listening'));
+const page = models.Page.sync();
+const user = models.User.sync();
+Promise.all([page, user])
+.then(() => {
+    console.log('database synced');
+    app.listen(3000, () => ('listening'));
+}).catch(console.error.bind(console));
 
 app.get('/', (req, res, next) => {
     res.render('index');
--- models/index.js
+++ models/index.js
@@ -1,2 +1,31 @@
 var Sequelize = require('sequelize');
-var db = new Sequelize('postgres://localhost:5432/wikistack');
+var db = new Sequelize('postgres://localhost:5432/wikistack', { logging: false });
+
+const Page = db.define('page', {
+    title: {
+        type: Sequelize.STRING
+    },
+    urlitle: {
+        type: Sequelize.STRING
+    },
+    content: {
+        type: Sequelize.TEXT
+    },
+    status: {
+        type: Sequelize.ENUM('open', 'close')
+    }
+});
+
+const User = db.define('user', {
+    name: {
+        type: Sequelize.STRING
+    },
+    email: {
+        type: Sequelize.STRING
+    }
+})
+
+module.exports = {
+    Page,
+    User
+}
