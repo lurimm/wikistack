600bcfacddac28d270bd10690e9d98ac923e7978
Aaron Young <brainomite@gmail.com>
2018-01-22 10:15:10 -0500

    setup finished, i think

--- app.js
+++ app.js
@@ -8,7 +8,11 @@ const app = express();
 
 app.set('view engine', 'html'); // have res.render work with html files
 app.engine('html', nunjucks.render); // when giving html files to res.render, tell it to use nunjucks
+
 nunjucks.configure('views', { noCache: true }) // point nunjucks to the proper directory for templates
+
+app.use(express.static('public')) //serve up static files
+
 
 app.listen(3000, () => console.log('im listening'));
 
